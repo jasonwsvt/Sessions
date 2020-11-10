d1c8e31eba16b8df28875954c280797992b540cb
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2020-11-10 18:09:48 -0500

    Troubleshooting

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+const { isValidObjectId } = require("mongoose");
+
 class App {
     _users = null;
     _buttons = null;
@@ -20,3 +22,51 @@ class App {
 }
 
 var app = new App();
+
+function isFunction(v)         { return v && {}.toString.call(v) === '[object Function]'; }
+function isString(v)           { return (typeof v == "string"); }
+function isArrayOfObjects(v)   { return (isArray(v) && v.forEach(x => isObject(x))); }
+function isArrayOfStrings(v)   { return (isArray(v) && v.forEach(x => isString(x))); }
+function isUndefined(v)        { return v == undefined; }
+function isNull(v)             { return v == null; }
+function isNumber(v)           { return (typeof v === 'number' && isFinite(v)); }
+function isInteger(v)          { return !isNaN(v) && parseInt(Number(v)) == v && !isNaN(parseInt(v, 10)); }
+function isBoolean(v)          { return Boolean(v); }
+function isSecondsFromEpoch(v) { return (isInteger(v) && v > 1600000000000); }
+
+function isArray(v) {
+    try { var j = JSON.stringify(v); }
+    catch (error) { return false; }
+    if (j.startsWith("[")) { return true; }
+    return false;
+}
+
+function isObject(v) {
+    try { var j = JSON.stringify(v); }
+    catch (error) { return false; }
+    if (j.startsWith("{")) { return true; }
+    return false;
+}
+
+function varType(v) {
+    if (isFunction(v))         { return "function"; }
+    if (isString(v))           { return "string"; }
+    if (isArrayOfObjects(v))   { return "arrayOfObjects"; }
+    if (isArrayOfStrings(v))   { return "arrayOfStrings"; }
+    if (isUndefined(v))        { return "undefined"; }
+    if (isNull(v))             { return "null"; }
+    if (isNumber(v))           { return "number"; }
+    if (isInteger(v))          { return "integer"; }
+    if (isBoolean(v))          { return "boolean"; }
+    if (isSecondsFromEpoch(v)) { return "secondsFromEpoch"; }
+    if (is(v)) { return ""; }
+    if (is(v)) { return ""; }
+}
+
+function varErr(v, func) {
+    var varType = varType(v);
+    var funcName = func.name;
+    var varName = Object.keys(v);
+    if (!func(v)) { console.log(funcName,"(",varName,") returned false. Is", varType,"\n", v);}
+}
+
