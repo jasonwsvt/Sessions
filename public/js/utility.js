fd86a8f4605ebd6ee287ae3e8641435931a0c4ea
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2020-11-13 22:26:12 -0500

    Sorting development

--- public/js/utility.js
+++ public/js/utility.js
@@ -4,12 +4,16 @@ class Utility {
     _group = null;
     _type = null;
     _naming = null;
+    _lastSortMethod = null;
 
     _caretDownIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-caret-down-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg>";
     _caretUpIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-caret-up-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z'/></svg>";
     _plusIcon = "<svg width='1.25em' height='1.25em' viewBox='0 0 16 16' class='bi bi-plus-square' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z'/><path fill-rule='evenodd' d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z'/></svg>";
     _pencilIcon = "<svg width='1.25em' height='1.25em' viewBox='0 0 16 16' class='bi bi-pencil-square' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/><path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/></svg>";
     _searchIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-search' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z'/><path fill-rule='evenodd' d='M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z'/></svg>";
+    _downArrow = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-arrow-down' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z'/></svg>";
+    _upArrow = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-arrow-up' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z'/></svg>";
+
 
     _pickerButtonID = null;
     _pickerDivID = null;
@@ -82,6 +86,11 @@ class Utility {
             });
 
             self.pickerDiv.on("click", function (e) {
+                e.stopPropagation();
+            });
+
+            self.pickerSort.find("button").on("click", function (e) {
+                self.pickerButtons(this.value);
                 e.stopPropagation();
             });
 //
@@ -203,10 +212,10 @@ class Utility {
         const pickerSearchInput = "<input id = '" + this._pickerSearchInputID + "' placeholder = 'search'>";
         const pickerScrollDiv = "<div id = '" + this._pickerScrollDivID + "'></div>";
         const pickerSort = "<div id = '" + this._pickerSortID + "' class='btn-group btn-group-sm' role='group'></div>";
-        const pickerSort1 = "<button type='button' class='btn btn-secondary'>Name</button>";
-        const pickerSort2 = "<button type='button' class='btn btn-secondary'>Creation</button>";
-        const pickerSort3 = "<button type='button' class='btn btn-secondary'>Edit</button>";
-        const pickerSort4 = "<button type='button' class='btn btn-secondary'>Open</button>";
+        const pickerSort1 = "<button type = 'button' class = 'btn btn-secondary' value = 'name'>A-Z</button>";
+        const pickerSort2 = "<button type = 'button' class = 'btn btn-secondary' value = 'creation'>Creation</button>";
+        const pickerSort3 = "<button type = 'button' class = 'btn btn-secondary' value = 'lastEdited'>Edited</button>";
+        const pickerSort4 = "<button type = 'button' class = 'btn btn-secondary' value = 'lastOpened'>Opened</button>";
 
         const renameButton = "<button id = '" + this._renameButtonID + "' type = 'button' class = 'btn btn-dark btn-sm'>" + pencilIcon + "</button>";
         const renameDiv = "<div id = '" + this._renameDivID + "' class = 'hidden'></div>";
@@ -231,24 +240,17 @@ class Utility {
 
     manage() {
         const self = this;
-        var code, pickerButtonText, scrollDivHeight;
-        this.pickerScrollDiv.empty();
+        var pickerButtonText, scrollDivHeight;
+//        this.pickerScrollDiv.empty();
 //        console.log("Group:", this.group);
 //        console.log("Current:", this.current);
 //        console.log("Current name:", this.current.name);
         pickerButtonText = this.current.name;
         if (this.entries > 1) { pickerButtonText += " " + this._caretDownIcon; }
         this.pickerButton.html(pickerButtonText);
+
         if (this.entries > 1) {
-            code = "<div style = 'display: grid'>";
-            this.group.sortByName.forEach(function(entry) {
-                code += "<div class = 'row'><button type='button' class='btn ";
-                if (entry.id == self.current.id) { code += "btn-info"; }
-                else { code += "btn-outline-info"; }
-                code += " btn-sm' value = '" + entry.id + "'>" + entry.name + "</button></div>";
-            });
-            code += "</div>";
-            this.pickerScrollDiv.append(code);
+            this.pickerButtons();
 
             this.pickerScrollDiv.find("button").on("click", function (e) {
                 self.group.findById(this.value).setAsCurrent();
@@ -270,6 +272,68 @@ class Utility {
 //            else                                               { this.addButton.attr("disabled", false); }
         }
 
+    }
+
+    pickerButtons(method = this._lastSortMethod) {
+        var code = "<div style = 'display: grid'>", sort;
+        const upArrow = this._upArrow;
+        const downArrow = this._downArrow;
+
+        if (method) { this._lastSortMethod = method; }
+        if (this._lastSortMethod == null) { this._lastSortMethod = "name"; }
+
+        switch (this._lastSortMethod.split("_")[0]) {
+            case "name":
+                sort = this.group.sortByName; console.log(sort);
+                if (this._lastSortMethod == "name") {
+                    this._lastSortMethod = "name_reverse";
+                    sort.reverse();
+                }
+                else {
+                    this._lastSortMethod = "name";
+                } 
+                break;
+            case "creation":
+                sort = this.group.sortByCreation;
+                if (this._lastSortMethod == "creation") {
+                    this._lastSortMethod = "creation_reverse";
+                    sort.reverse();
+                }
+                else {
+                    this._lastSortMethod = "creation";
+                }
+                break;
+            case "lastEdited":
+                sort = this.group.sortByLastEdited;
+                if (this._lastSortMethod == "lastEdited") {
+                    this._lastSortMethod = "lastEdited_reverse";
+                    sort.reverse();
+                }
+                else {
+                    this._lastSortMethod = "lastEdited";
+                }
+                break;
+            case "lastOpened":
+                sort = this.group.sortByLastOpened;
+                if (this._lastSortMethod == "lastOpened") {
+                    this._lastSortMethod = "lastOpened_reverse";
+                    sort.reverse();
+                }
+                else {
+                    this._lastSortMethod = "lastOpened";
+                }
+                break;
+        }
+
+        sort.forEach((entry) => {
+            code += "<div class = 'row'><button type='button' class='btn btn-block ";
+            if (entry.id == this.current.id) { code += "btn-info"; }
+            else { code += "btn-outline-info"; }
+            code += " btn-sm' value = '" + entry.id + "'>" + entry.name + "</button></div>";
+        });
+        code += "</div>";
+
+        this.pickerScrollDiv.html(code);
     }
 
     closeMenus(except) {
