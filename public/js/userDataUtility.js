d39eb2f0d6b9051266d4b735d0277272a5074332
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2020-12-11 11:22:54 -0500

    Troubleshooting / development

--- public/js/userDataUtility.js
+++ public/js/userDataUtility.js
@@ -12,14 +12,14 @@ class UserDataUtility {
     _plusIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-plus-square' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z'/><path fill-rule='evenodd' d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z'/></svg>";
     _minusIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-dash-square' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z'/><path fill-rule='evenodd' d='M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z'/></svg>";
     _hideIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-eye-slash' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z'/><path d='M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299l.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z'/><path d='M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709z'/><path fill-rule='evenodd' d='M13.646 14.354l-12-12 .708-.708 12 12-.708.708z'/></svg>";
-    _showIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-eye' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.134 13.134 0 0 0 1.66 2.043C4.12 11.332 5.88 12.5 8 12.5c2.12 0 3.879-1.168 5.168-2.457A13.134 13.134 0 0 0 14.828 8a13.133 13.133 0 0 0-1.66-2.043C11.879 4.668 10.119 3.5 8 3.5c-2.12 0-3.879 1.168-5.168 2.457A13.133 13.133 0 0 0 1.172 8z'/><path fill-rule='evenodd' d='M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z'/></svg>";
-    _expandIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-arrows-expand' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8zM7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10z'/></svg>";
-    _collapseIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-arrows-collapse' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8zm7-8a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 4.293V.5A.5.5 0 0 1 8 0zm-.5 11.707l-1.146 1.147a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 11.707V15.5a.5.5 0 0 1-1 0v-3.793z'/></svg>";
-    _trashIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-trash' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'/><path fill-rule='evenodd' d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'/></svg>";
+    _expandIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-fullscreen' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z'/></svg>";
+    _collapseIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-fullscreen-exit' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5zm5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5zM0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zm10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4z'/></svg>";
     _exportIcon = "<svg width='1.25em' height='1.25em' viewBox='0 0 16 16' class='bi bi-download' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z'/><path fill-rule='evenodd' d='M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z'/></svg>";
     _importIcon = "<svg width='1.25em' height='1.25em' viewBox='0 0 16 16' class='bi bi-upload' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z'/><path fill-rule='evenodd' d='M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z'/></svg>";
-    _downArrow = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-arrow-down' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z'/></svg>";
-    _upArrow = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-arrow-up' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z'/></svg>";
+    _downArrowIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-arrow-down' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z'/></svg>";
+    _upArrowIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-arrow-up' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z'/></svg>";
+    _squareIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-square' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z'/></svg>";
+    _checkedIcon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-check-square' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z'/><path fill-rule='evenodd' d='M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z'/></svg>"
 
     _divID = "userDataDiv";
     _buttonID = "userDataButton";
@@ -178,34 +178,34 @@ class UserDataUtility {
         this.options.data("sort_selectedClass", "btn-info");
         this.options.data("sort_0_state", 0);
         this.options.data("sort_0_states", 2);
-        this.options.data("sort_0_0_selected", this._downArrow + "A-Z");
+        this.options.data("sort_0_0_selected", this._downArrowIcon + "A-Z");
         this.options.data("sort_0_0_unselected", "A-Z");
         this.options.data("sort_0_0_value", "sort alphabetic descending");
-        this.options.data("sort_0_1_selected", this._upArrow + "A-Z");
+        this.options.data("sort_0_1_selected", this._upArrowIcon + "A-Z");
         this.options.data("sort_0_1_unselected", "A-Z");
         this.options.data("sort_0_1_value", "sort alphabetic ascending");
         this.options.data("sort_1_state", 0);
         this.options.data("sort_1_states", 2);
-        this.options.data("sort_1_0_selected", this._downArrow + "Creation");
+        this.options.data("sort_1_0_selected", this._downArrowIcon + "Creation");
         this.options.data("sort_1_0_unselected", "Creation");
         this.options.data("sort_1_0_value", "sort creation descending");
-        this.options.data("sort_1_1_selected", this._upArrow + "Creation");
+        this.options.data("sort_1_1_selected", this._upArrowIcon + "Creation");
         this.options.data("sort_1_1_unselected", "Creation");
         this.options.data("sort_1_1_value", "sort creation ascending");
         this.options.data("sort_2_state", 0);
         this.options.data("sort_2_states", 2);
-        this.options.data("sort_2_0_selected", this._downArrow + "Edited");
+        this.options.data("sort_2_0_selected", this._downArrowIcon + "Edited");
         this.options.data("sort_2_0_unselected", "Edited");
         this.options.data("sort_2_0_value", "sort edited descending");
-        this.options.data("sort_2_1_selected", this._upArrow + "Edited");
+        this.options.data("sort_2_1_selected", this._upArrowIcon + "Edited");
         this.options.data("sort_2_1_unselected", "Edited");
         this.options.data("sort_2_1_value", "sort edited ascending");
         this.options.data("sort_3_state", 0);
         this.options.data("sort_3_states", 2);
-        this.options.data("sort_3_0_selected", this._downArrow + "Opened");
+        this.options.data("sort_3_0_selected", this._downArrowIcon + "Opened");
         this.options.data("sort_3_0_unselected", "Opened");
         this.options.data("sort_3_0_value", "sort opened descending");
-        this.options.data("sort_3_1_selected", this._upArrow + "Opened");
+        this.options.data("sort_3_1_selected", this._upArrowIcon + "Opened");
         this.options.data("sort_3_1_unselected", "Opened");
         this.options.data("sort_3_1_value", "sort opened ascending");
         
@@ -514,15 +514,15 @@ console.log(this.scrollAreaDiv.position().left, this.div.width(), this.importBut
             }
         });
         keys = keys.concat(unsortedKeys);
-        keys.forEach(key => { console.log(key); });
+//        keys.forEach(key => { console.log(key); });
         
-        const rowButton = "<button type = 'button' class = 'btn btn-secondary rowButton'></button>";
-        const selectRecord = "<button type = 'button' class = 'btn btn-secondary selectRecord'></button>";
-        const childrenButton = "<button type = 'button' class = 'btn btn-secondary childrenButton'></button>";
-        const selectChildren = "<button type = 'button' class = 'btn btn-secondary selectChildren'></button>";
+        const rowButton = "<span class = 'rowButton'>" + this._expandIcon + "</span>";
+        const selectRecord = "<span class = 'selectRecord'>" + this._squareIcon + "</span>";
+        const childrenButton = "<span class = 'childrenButton'>" + this._expandIcon + "</span>";
+        const selectChildren = "<span class = 'selectChildren'>" + this._squareIcon + "</span>";
 
         var record = "", line;
-        console.log(local, imported, children, keys);
+//        console.log(local, imported, children, keys);
 
         keys.forEach((key, index) => {
             line = (index == 0) ? "<td class = 'outside" + tier + "'>" + rowButton + "</td>" : "<td></td>";
@@ -538,53 +538,28 @@ console.log(this.scrollAreaDiv.position().left, this.div.width(), this.importBut
         });
         if (children) {
             line = "<td class = 'outside" + tier + "'>" + childrenButton + "</td>";
-            line += "<td class = 'inside" + tier + "'>" + children + "</td>";
+            line += "<td class = 'inside" + tier + "'>" + children + ":</td>";
             [local, imported].forEach(record => {
                 if (isArray(record[children]) && record[children].length) {
                     line += "<td>(" + record[children].length + ")</td>";
                     line += "<td>" + selectChildren + "</td>";
                 }
-                else { line += "<td></td>"; }
+                else { line += "<td></td><td></td>"; }
             });
             record += "<tr>" + line + "</tr>";
         }
         this.scrollAreaDiv.append("<table id = 'row_" + id + "' class = 'flex-container'>" + record + "</table>");
-
-        /* keys.forEach((key, index) => {
-            line = "<div style = 'width: " + (30 + tier*50) + "px'>";
-            if (index == 0) { line += rowButton; }
-            line += "</div>";
-            line += "<div style = 'width: " + (200 - (30 + tier*50)) + "px'>" + key + "</div>";
-            [local, imported].forEach(record => {
-                if (record[key]) {
-                    line += "<div>" + record[key] + "</div>";
-                    line += (index == 0) ? "<div>" + selectRecord + "</div>" : "<div></div>";
-                }
-                else { line += "<div></div><div></div>"; }
-            });
-            record+= "<div>" + line + "</div>";
-        });
-        if (children) {
-            line = "<div style = 'width: " + (30 + tier*50) + "px'>" + childrenButton + "</div>";
-            line += "<div style = 'width: " + (500 - (30 + tier*50)) + "px'>" + children + "</div>";
-            [local, imported].forEach(record => {
-                if (isArray(record[children]) && record[children].length) {
-                    line += "<div>(" + record[children].length + ")" + selectChildren + "</div>";
-                }
-                else { line += "<div></div>"; }
-            });
-            record += "<div>" + line + "</div>";
-        }
-        this.scrollAreaDiv.append("<div id = 'row_" + id + "' class = 'flex-container'>" + record + "</div>");
-*/
+//console.log(local[children]);
         if (children) {
             keys = Object.keys(local[children]);
+//            console.log(keys);
             if (imported && Object.keys(imported).includes(children)) {
                 keys = keys.concat(imported[children]).filter(key, index => (keys.indexOf(key) === index));
             }
             keys.forEach(key => {
-                localRecord = (local && local[key]) ? local[key] : false;
-                importedRecord = (imported && imported[key]) ? imported[key] : false;
+                localRecord = (local[children][key]) ? local[children][key] : false;
+                importedRecord = (imported && imported[children] && imported[children][key]) ? imported[children][key] : false;
+                console.log(key, tier + 1, localRecord, importedRecord);
                 this._buildRecord(tier + 1, localRecord, importedRecord);
             });
         }
