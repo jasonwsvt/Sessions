cccfbd634aa03fe201b424ffc09cf64da80b2590
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-02-09 11:43:29 -0500

    List is working.

--- public/js/list.js
+++ public/js/list.js
@@ -21,35 +21,22 @@ class List {
                 //Automatic keys and value validation
                 this.add = (value) => {
                     if (this._validate(value, this._items)) {
-                        const key = this._items.length;
-                        this._items[key] = value;
+                        const key = parseInt(this._items.length);
+                        this._items.push({ "key": key, "value": value });
                         return key;
-                    }
-                }
-                this.remove = (value) => {
-                    const i = this.key(value);
-                    if (i != -1) {
-                        delete this._items[i];
-                        return true; }
-                    else {
-                        return false;
                     }
                 }
             }
             else {
                 //Manual keys and value validation
                 this.add = (key, value) => {
-                    if (isString(key) && !Object.keys(this._items).includes(key) && this._validate(value, this._items)) {
-                        this._items[key] = value;
+                    console.log(key, "is string:", isString(key));
+                    console.log(key, "is number:", isNumber(key));
+                    console.log(key, "is unique:", !this._items.find(item => item.key == key));
+                    console.log(value, "is valid:", this._validate(value, this._items));
+                    if ((isString(key) || isNumber(key)) && !this._items.find(item => item.key == key) && this._validate(value, this._items)) {
+                        this._items.push({ "key": key, "value": value });
                         return key;
-                    }
-                }
-                this.remove = (key) => {
-                    if (this.has(key)) {
-                        delete this._items[key];
-                        return true; }
-                    else {
-                        return false;
                     }
                 }
             }
@@ -59,53 +46,55 @@ class List {
                 //Automatic keys and no value validation
                 this.add = (value) => {
                     const key = this._items.length;
-                    this._items[key] = value;
+                    this._items.push({ "key": key, "value": value });
                     return key;
-            }
-                this.remove = (value) => {
-                    const i = this.key(value);
-                    if (i != -1) {
-                        delete this._items[i];
-                        return true; }
-                    else {
-                        return false;
-                    }
                 }
             }
             else {
                 //Manual keys and no value validation
                 this.add = (key, value) => {
-                    if (isString(key) && !Object.keys(this._items).includes(key)) {
-                        this._items[key] = value;
+                    if ((isString(key) || isNumber(key)) && !this._items.find(item => item.key == key)) {
+                        this._items.push({ "key": key, "value": value });
                         return key;
-                    }
-                }
-                this.remove = (key) => {
-                    if (this.has(key)) {
-                        delete this._items[key];
-                        return true; }
-                    else {
-                        return false;
                     }
                 }
             }
         }
         if (index == undefined) {
-            this._index = (value, values) => {
-                return values.findIndex(key => this._items[key] == value);
+            this._index = (value, items) => {
+                return items.findIndex(item => item.value == value);
             }
         }
     }
 
 
-    keys   = ()      => { return Object.keys(this._items); }
-    has    = (key)   => { return this.keys().includes(key); }
-    value  = (key)   => { return (this.has(key)) ? this.values()[this._items.indexOf(key)] : undefined; }
+    keys     = ()      => { return this._items.map(item => item.key); }
+    hasKey   = (key)   => { return !!this._items.find(item => item.key == key); }
+    value    = (key)   => { return this._items.find(item => item.key == key).value; }
+    dropKey  = (key)   => {
+//        console.log("dropKey", key, this.hasKey(key), this._items.findIndex(item => item.key == key));
+        if (this.hasKey(key)) {
+//            console.log("before", this.keys(), this.values());
+            this._items.splice(this._items.findIndex(item => item.key == key), 1);
+//            console.log("after", this.keys(), this.values());
+            return true; }
+        else {
+            return false;
+        }
+    }
     
-    values = ()      => { return Object.values(this._items); }
-    find   = (value) => { return this._index(value, this.values()) != -1; }
-    key    = (value) => { const i = this._index(value, this.values()); return (i != -1) ? this.keys()[i] : undefined; }
-    
-
+    values    = ()      => { return this._items.map(item => item.value); }
+    hasValue  = (value) => { return this._index(value, this._items) != -1; }
+    key       = (value) => { return this._items[this._index(value, this._items)].key; }
+    dropValue = (value) => {
+        const len = this._items.length;
+        var i = 0;
+        while (this.hasValue(value) && i++ < 10) {
+//            console.log("dropValue", value, this.hasValue(value), this._index(value, this._items));
+            this._items.splice(this._index(value, this._items), 1);
+        }
+        return (len != this._items.length);
+    }
+    dropValues = (values) => { values.forEach(value => this.dropValue(value)); } 
 }
     
