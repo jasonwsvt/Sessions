486ebc0ed029bdc772da1ec252a468b20faf7917
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-04-21 16:54:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -84,8 +84,11 @@ httpServer.listen(8080, () => {
 httpsServer.listen(8443, () => {
     console.log("Server has started on http://localhost:3000.");
 });
-*/
 
 app.listen(3000, "0.0.0.0", () => {
+    console.log("Server has started on http://localhost:3000.");
+});
+*/
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server has started on http://localhost:3000.");
 });
