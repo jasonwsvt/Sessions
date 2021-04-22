23486b3918163932e722c409e2cb67118005b9b9
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-04-22 17:05:30 -0400

    Update list.js
    I think this will work for the Safari issue.

--- public/js/list.js
+++ public/js/list.js
@@ -66,40 +66,40 @@ class List {
         if (index == undefined) {
             this._index = (value, items) => items.findIndex(item => item.value == value);
         }
-    }
 
-    keys     = ()      => { return this._items.map(item => item.key); }
-    hasKey   = (key)   => { return !!this._items.find(item => item.key == key); }
-    value    = (key)   => { return this._items.find(item => item.key == key).value; }
-    dropKey  = (key)   => {
-//        console.log("dropKey", key, this.hasKey(key), this._items.findIndex(item => item.key == key));
-        if (this.hasKey(key)) {
-//            console.log("before", this.keys(), this.values());
-            this._items.splice(this._items.findIndex(item => item.key == key), 1);
-//            console.log("after", this.keys(), this.values());
-            return true;
+        this.keys     = ()      => { return this._items.map(item => item.key); }
+        this.hasKey   = (key)   => { return !!this._items.find(item => item.key == key); }
+        this.value    = (key)   => { return this._items.find(item => item.key == key).value; }
+        this.dropKey  = (key)   => {
+    //        console.log("dropKey", key, this.hasKey(key), this._items.findIndex(item => item.key == key));
+            if (this.hasKey(key)) {
+    //            console.log("before", this.keys(), this.values());
+                this._items.splice(this._items.findIndex(item => item.key == key), 1);
+    //            console.log("after", this.keys(), this.values());
+                return true;
+            }
+            else {
+                return false;
+            }
         }
-        else {
-            return false;
+        
+        this.values    = ()       => { return this._items.map(item => item.value); }
+        this.hasValue  = (value)  => { return this._index(value, this._items) >= 0; }
+        this.key       = (value)  => { return this._items[this._index(value, this._items)].key; }
+        this.dropValue = (value)  => {
+            const len = this._items.length;
+            var i = 0;
+            while (this.hasValue(value) && i++ < 10) {
+    //            console.log("dropValue", value, this.hasValue(value), this._index(value, this._items));
+                this._items.splice(this._index(value, this._items), 1);
+            }
+            return (len != this._items.length);
         }
-    }
-    
-    values    = ()       => { return this._items.map(item => item.value); }
-    hasValue  = (value)  => { return this._index(value, this._items) >= 0; }
-    key       = (value)  => { return this._items[this._index(value, this._items)].key; }
-    dropValue = (value)  => {
-        const len = this._items.length;
-        var i = 0;
-        while (this.hasValue(value) && i++ < 10) {
-//            console.log("dropValue", value, this.hasValue(value), this._index(value, this._items));
-            this._items.splice(this._index(value, this._items), 1);
-        }
-        return (len != this._items.length);
-    }
-    dropValues = (values) => { values.forEach(value => this.dropValue(value)); } 
+        this.dropValues = (values) => { values.forEach(value => this.dropValue(value)); } 
 
-    clear     = ()       => { this._items = []; }
-    size      = ()       => { return this._items.length; }
-    isEmpty   = ()       => { return !this._items.length; }
+        this.clear     = ()       => { this._items = []; }
+        this.size      = ()       => { return this._items.length; }
+        this.isEmpty   = ()       => { return !this._items.length; }
+    }
 }
     
